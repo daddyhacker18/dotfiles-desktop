e5f8adb8a28da52b90244919766244403142dbed
daddyhacker18 <giles@mullins1.com>
2022-12-20 06:00:47 +0000

    change from macchina to neofetch and some other fish config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# set environment variables
+### set environment variables
 export ZYPP_MEDIANETWORK=1 
+export QT_QPA_PLATFORMTHEME=qt5ct
+export HISTCONTROL=ignoreboth       # remove duplicates in command history
 
-# set aliases
-alias ls='exa -lha'
-alias clear='clear && macchina'
+### set aliases
+alias ls='exa -lha'                 # expand ls to include detail and color
+alias clear='clear && neofetch'     # show neofetch on clear console 
+alias mv='mv -i'                    # prompt on move
+alias rm='rm -i'                    # prompt on remove
 
-# starship prompt
+### starship prompt
 starship init fish | source
 
-# display sysinfo
-macchina
+### display sysinfo
+# macchina
+neofetch
