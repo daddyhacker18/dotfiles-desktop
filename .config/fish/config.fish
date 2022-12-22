efb0670a3c3d46a41739c11fbaba4dda3282ed30
daddyhacker18 <giles@mullins1.com>
2022-12-22 06:51:55 +0000

    add alias for backup2nas

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,12 +6,14 @@ end
 export ZYPP_MEDIANETWORK=1 
 export QT_QPA_PLATFORMTHEME=qt5ct
 export HISTCONTROL=ignoreboth       # remove duplicates in command history
+fish_add_path path /home/giles/.config/emacs/bin/
 
 ### set aliases
 alias ls='exa -lha'                 # expand ls to include detail and color
 alias clear='clear && neofetch'     # show neofetch on clear console 
 alias mv='mv -i'                    # prompt on move
 alias rm='rm -i'                    # prompt on remove
+alias backup='sh /ntfs/tresorit/My\ Scripts/backup2nas.sh'
 
 ### starship prompt
 starship init fish | source
