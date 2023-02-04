65598c706c075f280351b610fa1f56ed8d853ab1
Giles Mullins <giles@mullins1.com>
2023-02-04 21:53:31 +0000

    fish - add snaps to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,8 @@ set fish_prompt_pwd_dir_length 0																												# expand full path i
 ### set path variables
 fish_add_path path /home/giles/.emacs.d/bin/																						# add emacs/doom to path
 fish_add_path path /home/giles/scripts/																									# add my own scripting directory to path
+fish_add_path path /home/giles/.local/bin																								# add local bin directory
+fish_add_path path /snap/bin                                                            # add snaps to path
 
 ### set aliases
 alias ls='exa -lha'																																			# expand ls to include detail and color
@@ -21,7 +23,7 @@ alias ls='exa -lha'																																			# expand ls to include det
 alias rm='echo "This is not the command you were looking for.  Use trash"; false'
 alias rmdir='echo "This is not the command you were looking for.  Use trash"; false'
 alias backup='sh /ntfs/tresorit/My\ Scripts/backup2nas.sh'
-alias vim='nvim'																																				# neovim to replace vi
+alias vim='lvim'																																				# neovim to replace vi
 alias yt-dlp-audio='yt-dlp -x --audio-format mp3'
 alias cryfs-open='cryfs /ntfs/tresorit/My\ Vault/CryFS ~/CryFS/'
 alias cryfs-close='cryfs-unmount "/home/giles/CryFS/"'
